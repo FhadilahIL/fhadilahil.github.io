0f2a07015e6083d4c6b8492f1bfb33210119c6d6
Muhammad Ilham Fhadilah <ilhammahier@gmail.com>
2024-09-13 17:32:41 +0700

    1.0.02

--- forms/contact.php
+++ forms/contact.php
@@ -24,14 +24,12 @@
   $contact->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
   $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
+    'host' => 'sandbox.smtp.mailtrap.io',
+    'username' => 'e83d7ada534571',
+    'password' => '8e69f5c53ff987',
     'port' => '587'
   );
-  */
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
